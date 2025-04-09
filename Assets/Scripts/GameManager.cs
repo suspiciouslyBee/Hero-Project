832a07f8b71622bad95d3d2ade8ee8cc47c4efcf
suspiciouslyBee <suspiciously@bee.movie>
2025-04-08 17:35:13 -0700

    added random spawning test

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,16 +4,37 @@ public class GameManager : MonoBehaviour
 {
     public GameObject enemy;
     public int enemyCount;
+    public float playableSpace = 0.9f;
+    public Camera playerCam;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        for(int i = 0; i < enemyCount; i++) {
+            SpawnEnemySomewhere();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void SpawnEnemySomewhere(){
+
+        //get the ranges by getting the playspace, then halving it to get one half of each axis
+        //range
+        float x = (Screen.width * playableSpace);
+        float y = (Screen.height * playableSpace);
+
+        
+
+
+        Instantiate(enemy, playerCam.ScreenToWorldPoint(new Vector3(Random.Range(0, x),
+                                                         Random.Range(0, y), 1))
+                            , Quaternion.identity);
+
+        return;
     }
 }
