9bfc960f1e293600cbcdb259935037334cc1de0f
suspiciouslyBee <suspiciously@bee.movie>
2025-04-12 15:03:28 -0700

    debugging still
    * moved placeholder init positon for objects

--- Assets/Scripts/DamageEnemyOnHit.cs
+++ Assets/Scripts/DamageEnemyOnHit.cs
@@ -19,7 +19,7 @@ public class DamageEnemyOnHit : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        Debug.Log("Hit Something");
 
 
         if (other.CompareTag("Enemy"))
