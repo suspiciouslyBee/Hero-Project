d09b34b7cc5272149969c4c7705aa7d3b9ce67ae
suspiciouslyBee <suspiciously@bee.movie>
2025-04-12 14:02:32 -0700

    drafted color change behaior

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,17 +3,36 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     private float health = 1;
+    private float maxHealth = 1;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(health < 0){
+
+        UpdateColor();
+
+
+        if (health < 0)
+        {
             Destroy(gameObject);
         }
+        
+    }
+
+    private void UpdateColor()
+    {
+        float normalizedAlpha = Mathf.Clamp((health / maxHealth) * 100, 0, 100);
+
+        //cant edit alpha directly must copy it temporarally
+        Color hopper = gameObject.GetComponent<SpriteRenderer>().color;
+
+        hopper.a = normalizedAlpha;
+
+        gameObject.GetComponent<SpriteRenderer>().color = hopper;
     }
 }
